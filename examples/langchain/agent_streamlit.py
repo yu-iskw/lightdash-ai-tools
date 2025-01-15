388ef109e6655698a281c852ce309a88703b8dfc
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2025-01-15 09:06:46 +0900

    tmp
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- examples/langchain/agent_streamlit.py
+++ examples/langchain/agent_streamlit.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, AnyMessage, HumanMessage, SystemMessage
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.graph import StateGraph
+from langgraph.graph import END, StateGraph
 from langgraph.prebuilt import create_react_agent
 from typing_extensions import TypedDict
 
@@ -28,49 +28,57 @@ from lightdash_ai_tools.langchain.tools import get_all_readable_tools
 from lightdash_ai_tools.lightdash.client import LightdashClient
 
 
-class LightdashOptAgentState(TypedDict):
+class LightdashOpsWorkflowState(TypedDict):
     """The state of the agent"""
     messages: List[AnyMessage]
     user_inputs: List[str]
     need_refine: bool
 
-    @classmethod
-    def get_initial_state(cls, initial_user_input: str) -> "LightdashOptAgentState":
-        """Get the initial state of the agent"""
-        return {"messages": [], "user_inputs": [initial_user_input], "need_fix": False}
+
+def get_initial_state(initial_user_input: str) -> LightdashOpsWorkflowState:
+    """Get the initial state of the agent"""
+    return {"messages": [], "user_inputs": [initial_user_input], "need_refine": False}
+
 
 class ReviewOutput(BaseModel):
     """The output of the review response"""
     solution: str = Field(..., description="The solution to the user's question")
     need_refine: bool = Field(..., description="Whether the solution needs to be refined")
 
-class LightdashOpsAgent:
+class FormatResponseOutput(BaseModel):
+    """The output of the format response"""
+    formatted_response: str = Field(..., description="The formatted response to the user's question")
+
+
+class LightdashOpsWorkflow:
     def __init__(self, lightdash_client: LightdashClient, llm: ChatGoogleGenerativeAI):
         self.lightdash_client = lightdash_client
         self.llm = llm  # Store the LLM instance for later use
         self.tools = get_all_readable_tools(lightdash_client=lightdash_client)
 
-    def get_graph_builder(self):
+    def get_graph_builder(self) -> StateGraph:
         """Get the graph builder for the agent"""
-        graph_builder = StateGraph(LightdashOptAgentState)
+        graph_builder = StateGraph(LightdashOpsWorkflowState)
         # Add the nodes to the graph
         tool_agent_node = self.tool_agent_node()
         graph_builder.add_node("tool_agent", tool_agent_node)
         graph_builder.add_node("review_agent", self.review_node)
+        graph_builder.add_node("format_response_agent", self.format_response_node)
         # Add the edges to the graph
         graph_builder.set_entry_point("tool_agent")
         graph_builder.add_edge("tool_agent", "review_agent")
         graph_builder.add_conditional_edges(
-          "review_agent",
-          self.should_continue,
-          {
-            True: "tool_agent",
-            False: "END",
-          }
+            "review_agent",
+            self.should_continue,
+            {
+                True: "tool_agent",
+                False: "format_response_agent",
+            }
         )
+        graph_builder.add_edge("format_response_agent", END)
         return graph_builder
 
-    def should_continue(self, state: LightdashOptAgentState):
+    def should_continue(self, state: LightdashOpsWorkflowState) -> bool:
         """Check if the agent should continue"""
         return state["need_refine"]
 
@@ -78,38 +86,63 @@ class LightdashOpsAgent:
         """Create the tool agent node"""
         return create_react_agent(self.llm, self.tools)
 
-    def review_node(self, state: LightdashOptAgentState):
+    def review_node(self, state: LightdashOpsWorkflowState) -> LightdashOpsWorkflowState:
         """Create the review node"""
         # Call the LLM with the messages
         messages = [
+            SystemMessage(content=textwrap.dedent("""\
+                You are a helpful reviewer to check if the current response answers the user's question.
+
+                If the response is enough, you will set the need_refine to False.
+                Otherwise, you will set the need_refine to True.
+                And, if it lacks tools to get the data, you will set the need_refine to False too.
+                That's because it is not possible to get the data without extra tools.
+
+                If the response is not enough, you will think of the solution to avoid the following pitfalls.
+
+                ## Common Pitfalls
+                - We have to pass the project UUID, not the project name, to get the data.
+                  So, it might be good to get all the projects so that we can identify the project UUID from the project name.
+                - We have to pass the user UUID, not the user name, to get the data.
+                  So, it might be good to get all the users so that we can identify the user UUID from the user name.
+            """.strip())),
+            state["messages"][-1]
+        ]
+        response = self.llm.with_structured_output(ReviewOutput).invoke(messages)
+        if isinstance(response, ReviewOutput):
+            # Update the state
+            state["messages"].append(AIMessage(content=response.solution))
+            state["need_refine"] = response.need_refine
+        else:
+            st.error("Unexpected result type from LLM.")
+        return state
+
+    def format_response_node(self, state: LightdashOpsWorkflowState) -> LightdashOpsWorkflowState:
+        """Format the response node"""
+        if not state["messages"]:  # Ensure there is a message to process
+            raise ValueError("No messages to format.")
+        print(state["messages"])
+        messages = [
           SystemMessage(content=textwrap.dedent("""\
-            You are a helpful reviewer to check if the current response answers the user's question.
-
-            If the response is enough, you will set the need_refine to False.
-            Otherwise, you will set the need_refine to True.
-            And, if it lacks tools to get the data, you will set the need_refine to False too.
-            That's because it is not possible to get the data without extra tools.
-
-            If the response is not enough, you will think of the solution to avoid the following pitfalls.
-
-            ## Common Pitfalls
-            - We have to pass the project UUID, not the project name, to get the data.
-              So, it might be good to get all the projects so that we can identify the project UUID from the project name.
-            - We have to pass the user UUID, not the user name, to get the data.
-              So, it might be good to get all the users so that we can identify the user UUID from the user name.
+            You are a skilled response formatter tasked with enhancing the clarity and presentation of the user's answer.
+            Your goal is to ensure that the response is well-structured, concise, and directly addresses the user's question.
+            For instance, it might be good to create a table or a list to make the response more readable.
           """.strip())),
           state["messages"][-1]
         ]
-        result = self.llm.with_structured_output(ReviewOutput).invoke(messages)
-        # Update the state
-        state["messages"].append(AIMessage(content=result.solution))
-        state["need_refine"] = result.need_refine
+        response = self.llm.with_structured_output(FormatResponseOutput).invoke(messages)
+        if isinstance(response, FormatResponseOutput):
+            # Update the state
+            state["messages"].append(AIMessage(content=response.formatted_response))
+        else:
+            raise ValueError("Unexpected result type from LLM.")
         return state
 
 
 def create_agent(lightdash_client: LightdashClient, llm: ChatGoogleGenerativeAI):
     """Create the agent with the tools"""
     return create_react_agent(llm, get_all_readable_tools(lightdash_client=lightdash_client))
+
 
 def main():
     # load env
@@ -121,7 +154,7 @@ def main():
     with st.sidebar:
         lightdash_url = st.text_input("Enter your Lightdash URL:", value=os.getenv("LIGHTDASH_URL", ""))
         lightdash_api_key = st.text_input("Enter your Lightdash API Key:", type="password", value=os.getenv("LIGHTDASH_API_KEY", ""))
-        google_ai_token = st.text_input("Enter your Google AI Studio Token:", type="password", value=os.getenv("GOOGLE_AI_TOKEN", ""))  # New field for Google AI token
+        google_ai_token = st.text_input("Enter your Google AI Studio Token:", type="password", value=os.getenv("GOOGLE_API_KEY", ""))  # New field for Google AI token
 
     # Use st.chat_input for the question input
     question = st.chat_input("Enter your question:")
@@ -129,7 +162,7 @@ def main():
     # Create Lightdash client
     if not lightdash_url or not lightdash_api_key or not google_ai_token:  # Check for Google AI token
         st.error("Please enter your LIGHTDASH_URL, LIGHTDASH_API_KEY, and GOOGLE_AI_TOKEN.")
-        return None
+        return
 
     client = LightdashClient(
         base_url=lightdash_url,
@@ -139,7 +172,7 @@ def main():
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=google_ai_token)
 
     if question:  # Check if question is provided
-        graph_builder = LightdashOpsAgent(client, llm).get_graph_builder()
+        graph_builder = LightdashOpsWorkflow(client, llm).get_graph_builder()
         workflow = graph_builder.compile()
         if workflow:
             while True:  # Loop to continue the chat
@@ -159,12 +192,15 @@ def main():
                         st.success("Response confirmed.")
                         break  # Exit the loop if confirmed
                     else:
-                        question = st.chat_input("Enter your next question:")  # Prompt for next input
-                        if not question:  # Exit loop if no new question is provided
+                        next_question = st.chat_input("Enter your next question:")  # Prompt for next input
+                        if next_question:
+                            question = next_question
+                        else:  # Exit loop if no new question is provided
                             st.warning("Please enter a question to proceed.")
                             break
     else:
         st.warning("Please enter a question to proceed.")
+
 
 if __name__ == "__main__":
     main()
